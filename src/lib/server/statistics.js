998a057a186b333a95ab5df6323bdbbcda969ee2
Pierre A. Dardignac <p.a.dardignac@gmail.com>
2023-12-12 10:22:45 +0100

    Fix statistics updat error

--- src/lib/server/statistics.js
+++ src/lib/server/statistics.js
@@ -14,29 +14,32 @@ const statistics = createLoader({
 });
 
 addSchedulerTask(async () => {
-  const torrents = await getTorrentList();
-  const previous = await statistics.get();
-  const now = Date.now();
-  const min = now - HISTORY_LIMIT;
+  const torrents = await getTorrentList().catch(() => {});
 
-  /** @type {import('@cyann/subler').Statistics} */
-  const next = {};
+  if (torrents) {
+    const previous = await statistics.get();
+    const now = Date.now();
+    const min = now - HISTORY_LIMIT;
 
-  for (let { name, status, progress } of torrents) {
-    if (status === 'seeding') {
-      const stats = previous[name] || [];
-      const first = stats.findIndex(({ timestamp }) => timestamp > min);
-      const last = stats[stats.length - 1];
+    /** @type {import('@cyann/subler').Statistics} */
+    const next = {};
 
-      if (!last || last.progress !== progress) {
-        stats.push({ timestamp: now, progress });
+    for (let { name, status, progress } of torrents) {
+      if (status === 'seeding') {
+        const stats = previous[name] || [];
+        const first = stats.findIndex(({ timestamp }) => timestamp > min);
+        const last = stats[stats.length - 1];
+
+        if (!last || last.progress !== progress) {
+          stats.push({ timestamp: now, progress });
+        }
+
+        next[name] = first > 0 ? stats.slice(first - 1) : stats;
       }
-
-      next[name] = first > 0 ? stats.slice(first - 1) : stats;
     }
-  }
 
-  statistics.set(next);
+    statistics.set(next);
+  }
 });
 
 /** @returns {Promise<import('@cyann/subler').Statistics>} */
